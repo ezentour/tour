5d3b63896dcbf63df986ab958c4f3c50be6ed63c
sanwonyu <havedreamm@naver.com>
2019-07-12 16:14:35 +0900

    cartList

--- ezentour/src/main/java/com/example/ezentour/controller/member/UserController.java
+++ ezentour/src/main/java/com/example/ezentour/controller/member/UserController.java
@@ -1,21 +1,32 @@
 package com.example.ezentour.controller.member;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.ezentour.model.user.dto.CartDTO;
+import com.example.ezentour.service.user.CartService;
 
 @Controller
 public class UserController {
 	private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
-
+	@Inject
+	CartService cartService;
 	@RequestMapping(value = "mypage/user/main")
 	public String home() {		
 		return "user/mypage/user_home";
 	}
 	
 	@RequestMapping(value = "mypage/user/mycart")
-	public String myCart() {		
+	public String myCart(Model model) {		
+		List<CartDTO> list = cartService.viewCartList();
+		model.addAttribute("list", list);
 		return "user/mypage/mycart";
 	}
 }
--- ezentour/src/main/java/com/example/ezentour/model/user/dao/CartDAO.java
+++ ezentour/src/main/java/com/example/ezentour/model/user/dao/CartDAO.java
@@ -6,6 +6,6 @@ import java.util.List;
 import com.example.ezentour.model.user.dto.CartDTO;
 
 public interface CartDAO {
-	public void insertCartList(int s_no,int h_no,String h_m_id,Date checkInDate,Date checkOutDate);
+	public void insertCartList(int s_no,int h_no,String h_m_id,String checkInDate,String checkOutDate);
 	public List<CartDTO> viewCartList();
 }
